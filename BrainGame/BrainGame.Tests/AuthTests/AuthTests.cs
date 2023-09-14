7e5825204e01b87a4d6b20174f53b8c1516bf737
Denchik23083 <deniskudravov228@gmail.com>
2023-09-14 16:25:35 +0300

    Auth tests is done

--- BrainGame/BrainGame.Tests/AuthTests/AuthTests.cs
+++ BrainGame/BrainGame.Tests/AuthTests/AuthTests.cs
@@ -1,12 +1,14 @@
-﻿using BrainGame.Db.Entities.Auth;
+﻿using BrainGame.Auth.Models;
+using BrainGame.Db.Entities.Auth;
 using BrainGame.Logic.AuthService;
+using BrainGame.Tests.Utilities;
 using BrainGame.WebDb.AuthRepository;
 using Moq;
 using Xunit;
 
 namespace BrainGame.Tests.AuthTests
 {
-    public class AuthTests
+    public class AuthTests : AuthUser
     {
         private readonly Mock<IAuthRepository> _repository;
 
@@ -33,6 +35,107 @@ namespace BrainGame.Tests.AuthTests
             await service.RegisterAsync(userModel);
 
             _repository.Verify(_ => _.RegisterAsync(userModel),
+                Times.Once);
+        }
+
+        [Fact]
+        public async Task Login()
+        {
+            var userModel = new User
+            {
+                Email = "user@gmail.com",
+                Password = "0000"
+            };
+
+            var user = User();
+
+            _repository.Setup(_ => _.LoginAsync(userModel))
+                .ReturnsAsync(user);
+
+            IAuthService service = new AuthService(_repository.Object);
+
+            var result = await service.LoginAsync(userModel);
+
+            _repository.Verify(_ => _.LoginAsync(userModel),
+                Times.Once);
+
+            Assert.Equal(user, result);
+        }
+
+        [Fact]
+        public async Task Refresh()
+        {
+            var user = User();
+
+            var refreshTokenModel = new RefreshToken
+            {
+                Value = new Guid(),
+                UserId = user.Id
+            };
+
+            var refreshToken = new RefreshToken
+            {
+                Value = new Guid(),
+                UserId = user.Id,
+                User = user
+            };
+
+            _repository.Setup(_ => _.RefreshAsync(refreshTokenModel))
+                .ReturnsAsync(refreshToken);
+
+            IAuthService service = new AuthService(_repository.Object);
+
+            var result = await service.RefreshAsync(refreshTokenModel);
+
+            _repository.Verify(_ => _.RefreshAsync(refreshTokenModel),
+                Times.Once);
+
+            Assert.Equal(user, result);
+        }
+
+        [Fact]
+        public async Task CreateRefreshToken()
+        {
+            var user = User();
+
+            var refreshToken = new Guid();
+
+            user.RefreshToken = new RefreshToken
+            {
+                Value = refreshToken
+            };
+
+            _repository.Setup(_ => _.CreateRefreshTokenAsync(It.IsAny<User>()));
+
+            IAuthService service = new AuthService(_repository.Object);
+
+            await service.CreateRefreshTokenAsync(refreshToken, user);
+
+            _repository.Verify(_ => _.CreateRefreshTokenAsync(It.IsAny<User>()),
+                Times.Once);
+        }
+
+        [Fact]
+        public async Task UpdateRefreshToken()
+        {
+            var user = User();
+
+            var refreshToken = new Guid();
+
+            user.RefreshToken = new RefreshToken
+            {
+                Value = refreshToken
+            };
+
+            user.RefreshToken!.Value = refreshToken;
+
+            _repository.Setup(_ => _.UpdateRefreshTokenAsync(It.IsAny<User>()));
+
+            IAuthService service = new AuthService(_repository.Object);
+
+            await service.UpdateRefreshTokenAsync(refreshToken, user);
+
+            _repository.Verify(_ => _.UpdateRefreshTokenAsync(It.IsAny<User>()),
                 Times.Once);
         }
     }
---
+++ BrainGame/BrainGame.Tests/Utilities/AuthUser.cs
@@ -0,0 +1,20 @@
+﻿using BrainGame.Db.Entities.Auth;
+
+namespace BrainGame.Tests.Utilities
+{
+    public class AuthUser
+    {
+        protected static User User()
+        {
+            return new User
+            {
+                Id = 3,
+                Name = "Anna",
+                Email = "user@gmail.com",
+                Password = "0000",
+                GenderId = 2,
+                RoleId = 3
+            };
+        }
+    }
+}
